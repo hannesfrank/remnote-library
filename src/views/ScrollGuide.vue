b8bdeac3574ea8c4f7f61920d88168f7a11df90e
Hannes Frank <frankhannes@gmail.com>
2020-11-29 17:14:49 +0100

    Add scroll guide placeholder

--- src/views/ScrollGuide.vue
+++ src/views/ScrollGuide.vue
@@ -1,7 +1,37 @@
 <template>
-  <div class="scroll-guide">
-    <h1>Publish to RemNote Library</h1>
-
-    Make and share your own scrolls.
+  <div class="scroll-guide content">
+    <h2 class="is-size-2">Make and Share a Scroll</h2>
+    <figure class="image is-300x300 mt-0 is-pulled-right">
+      <img
+        src="../assets/scroll-bound.svg"
+        alt="bound scroll"
+      />
+    </figure>
+    <p class="is-size-4 is-family-primary">
+      Comming Soon...
+    </p>
+    <p>
+      Basically you
+      <ol>
+        <li>Copy the Scroll template</li>
+        <li>Add your "magic", e.g. a some awesome CSS</li>
+        <li>Describe it in <code>manifest.json</code> and</li>
+        <li>Make a Pull Request on Github.</li>
+      </ol>
+    </p>
+    <p>Later we'll add a backend with a database and a UI to publish scrolls.</p>
   </div>
 </template>
+
+<style scoped>
+.scroll-guide {
+  padding: 30px;
+}
+
+img {
+  margin: 0 50px;
+  width: 300px;
+  height: 200px;
+  object-fit: cover;
+}
+</style>
